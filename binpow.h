8ebe4a4fd29cfdbeb0b4a48af75207993634e0f7
Your Name <you@example.com>
2021-02-07 17:22:10 +0500

    del

--- binpow.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <iostream>
-#ifndef BINPOW_H
-#define BINPOW_H
-
-unsigned int binpow(unsigned int a,unsigned int b ,unsigned int mod);
-unsigned int mul (unsigned int a,unsigned int b,unsigned int mod);
-
-#endif
